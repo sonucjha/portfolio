d4e870695d059dfcfb2a63239eebd6f4be2cdbe0
sonucjha <nabnitcjha@gmil.com>
2023-09-20 16:48:57 +0545

    improve code

--- resources/js/components/BeforeLoginPage.vue
+++ resources/js/components/BeforeLoginPage.vue
@@ -1,8 +1,11 @@
 <template>
+  <div>
+    <login></login>
+  </div>
   <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top d-flex align-items-center">
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="" />
         <span class="d-none d-lg-block">NiceAdmin</span>
       </a>
@@ -12,9 +15,9 @@
     <nav class="header-nav ms-auto">
       <ul class="d-flex align-items-center">
         <li class="nav-item dropdown pe-3">
-          <a href="index.html" class="logo d-flex align-items-center w-auto">
-                <span class="d-none d-lg-block">Log In</span>
-              </a>
+          <a @click.stop="showModel" class="logo d-flex align-items-center w-auto">
+            <span class="d-none d-lg-block">Log In</span>
+          </a>
         </li>
         <!-- End Profile Nav -->
       </ul>
@@ -26,7 +29,6 @@
   <main id="main" class="main bf-main">
     <div class="pagetitle">
       <h1>Blank Page</h1>
-      
     </div>
     <!-- End Page Title -->
 
@@ -61,6 +63,17 @@
   <!-- End #main -->
 </template>
 
+<script setup>
+import Login from "./Login.vue";
+import { ref } from "vue";
+
+const showLoginModel = ref(false);
+
+ function showModel(){
+  showLoginModel.value = true
+ }
+
+</script>
 
 <style>
 @media (min-width: 1200px) {
