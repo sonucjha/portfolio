1887daba1fc34914ada1caa9d991e9e5322e7631
sonucjha <nabnitcjha@gmil.com>
2023-09-21 11:02:38 +0545

    create modal for login

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -17,131 +17,91 @@
             <div class="modal fade" id="verticalycentered" tabindex="-1">
               <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title">Vertically Centered</h5>
-                    <button
-                      type="button"
-                      class="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                    ></button>
-                  </div>
                   <!-- login code section start -->
-                  <div class="container">
-                    <section
-                      class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4"
+                  <div class="container nopadding">
+                    <div
+                      class="col-12 d-flex flex-column align-items-center justify-content-center"
                     >
-                      <div class="container">
-                        <div class="row justify-content-center">
-                          <div
-                            class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center"
-                          >
-                            <div class="d-flex justify-content-center py-4">
-                              <a
-                                href="index.html"
-                                class="logo d-flex align-items-center w-auto"
+                      <div class="card mb-3 noshadow">
+                        <div class="card-body">
+                          <div class="pt-4 pb-2">
+                            <h5 class="card-title text-center pb-0 fs-4">
+                              Login to Your Account
+                            </h5>
+                            <p class="text-center small">
+                              Enter your username & password to login
+                            </p>
+                          </div>
+
+                          <form class="row g-3 needs-validation" novalidate>
+                            <div class="col-12">
+                              <label for="yourUsername" class="form-label"
+                                >Username</label
                               >
-                                <img src="assets/img/logo.png" alt="" />
-                                <span class="d-none d-lg-block">NiceAdmin</span>
-                              </a>
-                            </div>
-                            <!-- End Logo -->
-
-                            <div class="card mb-3">
-                              <div class="card-body">
-                                <div class="pt-4 pb-2">
-                                  <h5 class="card-title text-center pb-0 fs-4">
-                                    Login to Your Account
-                                  </h5>
-                                  <p class="text-center small">
-                                    Enter your username & password to login
-                                  </p>
+                              <div class="input-group has-validation">
+                                <span class="input-group-text" id="inputGroupPrepend"
+                                  >@</span
+                                >
+                                <input
+                                  type="text"
+                                  name="username"
+                                  class="form-control"
+                                  id="yourUsername"
+                                  required
+                                />
+                                <div class="invalid-feedback">
+                                  Please enter your username.
                                 </div>
-
-                                <form class="row g-3 needs-validation" novalidate>
-                                  <div class="col-12">
-                                    <label for="yourUsername" class="form-label"
-                                      >Username</label
-                                    >
-                                    <div class="input-group has-validation">
-                                      <span
-                                        class="input-group-text"
-                                        id="inputGroupPrepend"
-                                        >@</span
-                                      >
-                                      <input
-                                        type="text"
-                                        name="username"
-                                        class="form-control"
-                                        id="yourUsername"
-                                        required
-                                      />
-                                      <div class="invalid-feedback">
-                                        Please enter your username.
-                                      </div>
-                                    </div>
-                                  </div>
-
-                                  <div class="col-12">
-                                    <label for="yourPassword" class="form-label"
-                                      >Password</label
-                                    >
-                                    <input
-                                      type="password"
-                                      name="password"
-                                      class="form-control"
-                                      id="yourPassword"
-                                      required
-                                    />
-                                    <div class="invalid-feedback">
-                                      Please enter your password!
-                                    </div>
-                                  </div>
-
-                                  <div class="col-12">
-                                    <div class="form-check">
-                                      <input
-                                        class="form-check-input"
-                                        type="checkbox"
-                                        name="remember"
-                                        value="true"
-                                        id="rememberMe"
-                                      />
-                                      <label class="form-check-label" for="rememberMe"
-                                        >Remember me</label
-                                      >
-                                    </div>
-                                  </div>
-                                  <div class="col-12">
-                                    <button class="btn btn-primary w-100" type="submit">
-                                      Login
-                                    </button>
-                                  </div>
-                                  <div class="col-12">
-                                    <p class="small mb-0">
-                                      Don't have account?
-                                      <a href="pages-register.html">Create an account</a>
-                                    </p>
-                                  </div>
-                                </form>
                               </div>
                             </div>
-                          </div>
+
+                            <div class="col-12">
+                              <label for="yourPassword" class="form-label"
+                                >Password</label
+                              >
+                              <input
+                                type="password"
+                                name="password"
+                                class="form-control"
+                                id="yourPassword"
+                                required
+                              />
+                              <div class="invalid-feedback">
+                                Please enter your password!
+                              </div>
+                            </div>
+
+                            <div class="col-12">
+                              <div class="form-check">
+                                <input
+                                  class="form-check-input"
+                                  type="checkbox"
+                                  name="remember"
+                                  value="true"
+                                  id="rememberMe"
+                                />
+                                <label class="form-check-label" for="rememberMe"
+                                  >Remember me</label
+                                >
+                              </div>
+                            </div>
+                            <div class="col-12">
+                              <button class="btn btn-primary w-100" type="submit">
+                                Login
+                              </button>
+                            </div>
+                            <div class="col-12">
+                              <p class="small mb-0">
+                                Don't have account?
+                                <a href="pages-register.html">Create an account</a>
+                              </p>
+                            </div>
+                          </form>
                         </div>
                       </div>
-                    </section>
+                    </div>
                   </div>
                   <!-- login code section end -->
-                  <div class="modal-footer">
-                    <button
-                      type="button"
-                      class="btn btn-secondary"
-                      data-bs-dismiss="modal"
-                    >
-                      Close
-                    </button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                  </div>
                 </div>
               </div>
             </div>
