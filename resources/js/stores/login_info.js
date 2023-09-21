73552d3692ad53882b7b216661206c9fbfb2fc4d
sonucjha <nabnitcjha@gmil.com>
2023-09-21 21:45:52 +0545

    improve code

--- resources/js/stores/login_info.js
+++ resources/js/stores/login_info.js
@@ -1,32 +1,38 @@
 // stores/counter.js
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const loginInfoStore = defineStore(
-{
- id:'loginInfoStoreId',
-  state: () => {
-    return { 
-      loginInfo: {},
-      isAuthenticate:false 
-    }
-  },
-  getters:{
-    getLoginInfo(){
-        return this.loginInfo;
+export const loginInfoStore = defineStore({
+    id: "loginInfoStoreId",
+    state: () => {
+        return {
+            loginInfo: {},
+            isAuthenticate: false,
+            token: "",
+        };
     },
-    getAuthenticate(){
-      return this.isAuthenticate;
-    }
-  },
-  actions: {
-    setLoginInfo(val) {
-      this.loginInfo=val;
+    getters: {
+        getToken() {
+            return this.token;
+        },
+        getLoginInfo() {
+            return this.loginInfo;
+        },
+        getAuthenticate() {
+            return this.isAuthenticate;
+        },
     },
-    setAuthenticate(val){
-      this.isAuthenticate=val;
-    }
-  },
-  persist: {
-    enabled: true
-  }
-})
+    actions: {
+        setLoginInfo(val) {
+            this.loginInfo = val;
+        },
+        setAuthenticate(val) {
+            this.isAuthenticate = val;
+        },
+        setToken(val){
+          this.token = val
+        }
+    },
+    persist: {
+        enabled: true,
+    },
+});
