37f3c714d4a535ec770853eaacb68babc0525b37
sonucjha <nabnitcjha@gmil.com>
2023-10-12 09:57:24 +0545

    insert into intro section

--- app/Http/Controllers/IntroController.php
+++ app/Http/Controllers/IntroController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Introduction;
 
 class IntroController extends Controller
 {
     //
     public function saveIntro(Request $request){
-        
-        return 'sonu jha';
+         $intro = new Introduction;
+         $intro->name = $request->name;
+         $intro->position = $request->position;
+         $intro->facebook = $request->facebook;
+         $intro->twitter = $request->twitter;
+         $intro->instagram = $request->instagram;
+         $intro->save();
+        return $intro;
     }
 }
